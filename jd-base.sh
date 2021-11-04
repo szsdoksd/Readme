8a537e4a858e60e9e71046041cdc3e2de0239d50
szsdoksd <76995657+szsdoksd@users.noreply.github.com>
2021-11-04 18:07:04 +0800

    Update jd-base.sh

--- jd-base.sh
+++ jd-base.sh
@@ -10,13 +10,13 @@ echo "
 \______|    \/               \/     \/     \/     \/
                                                                                                   
 "
-DOCKER_IMG_NAME="shuye72/jd-base"
+DOCKER_IMG_NAME="wuzhi05/MyActions"
 JD_PATH=""
 SHELL_FOLDER=$(pwd)
 CONTAINER_NAME=""
 CONFIG_PATH=""
 LOG_PATH=""
-TAG="gitee"
+TAG="github"
 
 HAS_IMAGE=false
 PULL_IMAGE=true
